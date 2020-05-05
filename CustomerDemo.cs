056493668f22b8d495c5ab4f98db7489e2d43222
jainandhinis <jainandhinis@gmail.com>
2020-05-05 18:06:52 -0500

    runtime polymorphism

--- CustomerDemo.cs
+++ CustomerDemo.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 namespace basics{
 
     interface ISubscription{
         public void AddSubscription();
+        public void RemoveSubscription();
     }
     abstract class Customer{
         protected string name;
@@ -17,7 +19,7 @@ namespace basics{
         const string Netflix="Netflix";
         const string Hulu="Hulu";
         const string Prime="Prime";
-        string[] ott=new string[3];
+        string[] ott=new string[3]; //using simple array to hold list of data
         int count=0; 
         //enum channels {Netflix=1,Hulu,Prime};
         public OTTCustomer():base(){
@@ -25,6 +27,7 @@ namespace basics{
             Console.WriteLine("Inside child: OTTCustomer");
         }
         public void AddSubscription(){
+            Console.WriteLine("**Add OTT Subscription**");
             int option;
             string choice;
             do{
@@ -48,9 +51,69 @@ namespace basics{
             
             
         }
+        public void RemoveSubscription(){}
         public override void display(){
             Console.WriteLine("OTT subscribed to by the customer {0}",name);
                 foreach(string s in ott){
+                    Console.WriteLine(s);
+                }
+        }
+        //polymorphism example with same method name and diff args
+        public void display(String msg){
+                Console.WriteLine(msg);
+        }
+    }
+     class PodcastList: Customer,ISubscription{
+        ArrayList plist=new ArrayList(); //using Collections list to hold list of data
+        const string spotify="Spotify";
+        const string audible="Audible";
+        const string google="Google Podcasts";
+        public PodcastList(){
+            Console.WriteLine("Inside podcast constructor");
+        }
+        public string getUserChoice(ref int option){
+            string choice="";
+            Console.WriteLine("\n1.Spotify 2.Audible 3.Google Podcasts 4.Exit Menu \nEnter your option: ");
+            option=Convert.ToInt32(Console.ReadLine());
+            switch(option){
+                case 1:
+                    choice=spotify; break;
+                case 2:
+                    choice=audible; break;
+                case 3:
+                    choice=google; break;}
+            return choice;
+        }
+        public void AddSubscription(){
+            Console.WriteLine("**Add Podcast Subscription**");
+            string choice;
+            int option=0;
+            do{
+                    choice=getUserChoice(ref option);
+                    if(choice!=""){
+                    plist.Add(choice);
+                    Console.WriteLine("{0} has been added!",choice);
+                    }
+            }while(option!=4);
+            
+        }
+        public void RemoveSubscription(){
+            Console.WriteLine("**Remove Podcast Subscription**");
+            string choice;
+            int option=0;
+            do{
+                    choice=getUserChoice(ref option);
+                    if(choice!=""){
+                    plist.Remove(choice);
+                    Console.WriteLine("{0} has been removed!",choice);
+                    }
+            }while(option!=4);
+            
+        }
+            
+        public override void display(){
+            Console.WriteLine("Podcast subscribed to by the customer {0}",name);
+                foreach(string s in plist){
                     Console.WriteLine(s);
                 }
         }
@@ -60,12 +123,23 @@ namespace basics{
     }
     public class CustomerDemo{
             public CustomerDemo(){
+                Customer customer;
                 Console.WriteLine("***************Customer  demo- Inheritance,Polymorphism,Interface*********");
                 OTTCustomer oTT=new OTTCustomer();
+                customer=oTT; //runtime polymorphism
                 oTT.display("Welcome to OTT subscription services");
                 oTT.AddSubscription();
-                oTT.display();
+                customer.display();
                 oTT.display("Thank you!!");
+
+                PodcastList podcastList=new PodcastList();
+                customer=podcastList;
+                podcastList.AddSubscription();
+                customer.display(); //runtime polymorphism
+                podcastList.RemoveSubscription();
+                customer.display();
+                podcastList.display("Thank you!!");
+
 
 
             }
